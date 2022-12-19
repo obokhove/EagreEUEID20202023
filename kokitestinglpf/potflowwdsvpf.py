3ee8271d17d7612e1136d9ab0d3bfddd645055e2
O Bokhove <o.bokhove@leeds.ac.uk>
2022-12-19 17:36:28 +0000

    Add files via upload

--- kokitestinglpf/potflowwdsvpf.py
+++ kokitestinglpf/potflowwdsvpf.py
@@ -15,7 +15,7 @@ Lx = 20.0  # length of the tank [m] in x-direction; needed for computing initial
 Lz = 10.0  # height of the tank [m]; needed for computing initial condition
 H0 = Lz # rest water depth [m]
 
-nx = 4*120
+nx = 120
 nz = 6
 
 # control parameters
@@ -27,7 +27,7 @@ if not os.path.exists(save_path):
 
 top_id = 4
 
-nvpcase = 0 # ONNO 07-12 to 18-12: choice 0: standard weak-form approach with 3 steps 1: VP approach with two steps
+nvpcase = 2 # ONNO 07-12 to 18-12: choice 0: standard weak-form approach with 3 steps 1: VP approach with two steps
 
 #__________________  FIGURE PARAMETERS  _____________________#
 
@@ -64,7 +64,7 @@ b = 0.005 * T / L ** 2  # in nondim units
 
 kx = 2 * np.pi * n_mode / Lx
 omega = np.sqrt(gg * kx * np.tanh(kx * Lz))
-A = 1.0
+A = 0.2
 D = -gg*A/(omega*np.cosh(kx*H0))
 
 Tperiod = 2*np.pi/omega
@@ -78,10 +78,11 @@ phi_exact_expr = D * fd.cos(kx * x[0]) * fd.cosh(kx * x[1]) * np.sin(omega * t0)
 eta_exact_expr = A * fd.cos(kx * x[0]) * np.cos(omega * t0)
 
 t_end = Tperiod  # time of simulation [s]
-dtt = Lx/nx/np.sqrt(gg*H0) # i.e. dx/max(c0) with c0 =sqrt(g*H0)
-Nt = 2*2*200 # check with print statement below and adjust dt towards dtt vi Nt halving time step seems to half energy oscillations
+dtt = np.minimum(Lx/nx,Lz/nz)/(np.pi*np.sqrt(gg*H0)) # i.e. dx/max(c0) with c0 =sqrt(g*H0)
+Nt = 100 # check with print statement below and adjust dt towards dtt vi Nt halving time step seems to half energy oscillations
 dt = Tperiod/Nt  # 0.005  # time step [s]
-print('dtt=',dtt, t_end/dtt,dt)
+dt = dtt
+print('dtt=',dtt, t_end/dtt,dt,2/omega)
 
 
 ##______________  To get results at different time steps ______________##
@@ -97,7 +98,7 @@ t2 = int(len(time)/2)
 nt = int(len(time)/nplot)
 t_plot = np.array([ time[0], time[t2], time[-1] ])
 t_plot = time[0::nt]
-print('t_plot', t_plot,nt,nplot)
+print('t_plot', t_plot,nt,nplot, t_end)
 print('gg:',gg)
 color = np.array(['g-', 'b--', 'r:', 'm:'])
 colore = np.array(['k:', 'c--', 'm:'])
@@ -113,6 +114,8 @@ if nvpcase == 0:
     ax1.set_title(r'Weak form used:',fontsize=tsize)
 elif nvpcase == 1:
     ax1.set_title(r'Functional derivative VP used:',fontsize=tsize)
+elif nvpcase == 2:
+    ax1.set_title(r'VP nonlinear case used:',fontsize=tsize)
 # end if
 ax1.set_ylabel(r'$\eta (x,t) [m]$ ',fontsize=size)
 ax1.grid()
@@ -133,7 +136,10 @@ phi_f = fd.Function(V_W, name="phi_f")  # at the free surface
 phif_new = fd.Function(V_W, name="phi_f") 
 
 eta = fd.Function(V_W, name="eta")
-eta_new = fd.Function(V_W, name="eta")
+eta_new = fd.Function(V_W, name="eta_new")
+
+heta = fd.Function(V_W, name="heta")
+h_new = fd.Function(V_W, name="h_new")
 
 trial_W = fd.TrialFunction(V_W)
 trial_eta = fd.TrialFunction(V_W)
@@ -225,14 +231,38 @@ elif nvpcase==1:
     # Step-3: f-derivative wrt phi but restrict to free surface to find updater eta_new; only solve for eta_new by using exclude
     eta_expr2 = fd.derivative(VP, phi, du=v_W)
     eta_expr = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(eta_expr2,eta_new,bcs=BC_exclude_beyond_surface))
-elif nvpcase==2:
+elif nvpcase==2: # ONNO 19-12
     # 
     # Desired VP format of the above
     # 
     # VP formulation of above with phi^n+1=phi_f at free top surface the same but interior phi^(n+1) and surface eta^(n+1) in one go
     #
-    VP =  ( fd.inner(trial_phi, (trial_eta - eta)/dt) + fd.inner(phi, eta/dt) - (1/2 * g * fd.inner(eta,eta)) )* fd.ds(top_id) \
-        + ( - 1/2 * H0 * fd.inner(fd.grad(trial_phi), fd.grad(trial_phi))  ) * fd.dx
+    Lw = 0.5*Lx
+    Ww = Lw # Later wavemaker to be added
+    # eta_new -> h_new and eta -> heta ; Nonlinear potential-flow VP:
+    fac = 1.0 # now same as linear case above except for constant pref-factors as check; seems same ONNO 19-12: convergence fails FAILS! with 1.0
+    VPnl = ( H0*Ww*fd.inner(phi, (eta_new - eta)/dt) + H0*Ww*fd.inner(phi_f, eta/dt) - gg*Ww*H0*(0.5*fd.inner(H0+eta, H0+eta)-(H0+eta)*H0+0.5*H0**2) )* fd.ds(top_id) \
+        - 1/2 * ( (Lw**2/Ww) * (H0+fac*eta) * (phi.dx(0)-(z/(H0+fac*eta))*fac*eta.dx(0)*phi.dx(1))**2 + Ww * (H0**2/(H0+fac*eta)) * (phi.dx(1))**2 ) * fd.dx
+    #  Step-1: only nonlinear step just trying these solver_parameter!
+    param_h       = {'ksp_converged_reason':None, 'pc_type': 'fieldsplit','pc_fieldsplit_type': 'schur','pc_fieldsplit_schur_fact_type': 'upper'}  
+    param_psi2    = {'ksp_converged_reason':None, 'ksp_type': 'preonly', 'pc_type': 'lu'}
+    param_hat_psi2 = {'ksp_converged_reason':None, 'ksp_type': 'preonly', 'pc_type': 'lu'}
+    param_hat_psi3 = {'snes_type': 'newtonls','ksp_type': 'preonly', 'pc_type': 'lu'}
+    param_psi    = {'ksp_type': 'preonly', 'pc_type': 'lu'}
+    param_psi3 = {'ksp_type': 'richardson', 'pc_type': 'jacobi'}
+    param_hat_psi4 = {'ksp_converged_reason':None, 'snes_type': 'newtonls','ksp_type': 'richardson', 'pc_type': 'jacobi'} #   17 IC in 1 test
+    param_hat_psi5 = {'ksp_converged_reason':None, 'snes_type': 'newtonls','ksp_type': 'gmres', 'pc_type': 'jacobi'} #  2 or 3 to 8 or 9 in 1 (same) test; goes up then fails
+    param_hat_psi6 = {'ksp_converged_reason':None, 'snes_type': 'newtonls','ksp_type': 'gmres', 'pc_type': 'lu'} #  1 iteration but then blows up again at some point
+    param_hat_psi = {'snes_type': 'newtonls','ksp_type': 'gmres', 'pc_type': 'lu'} #  1 iteration but then blows up again at some point
+    
+    phif_exprnl1 = fd.derivative(VPnl, eta, du=v_W) # du=v_W represents perturbation seems that with these solver_parameters solves quicker: tic-toc it with and without?
+    phif_exprnl = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(phif_exprnl1, phi, bcs=BC_exclude_beyond_surface), solver_parameters=param_hat_psi6)
+    #  Step-2: linear solve; ONNO 19-12: how does one enforce that?
+    phi_exprnl1 = fd.derivative(VPnl, phi, du=v_W)
+    phi_exprnl = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(phi_exprnl1, phi, bcs = BC_phi), solver_parameters=param_psi) 
+    #  Step-3: linear solve; ONNO 19-12: how does one enforce that?
+    heta_exprnl2 = fd.derivative(VPnl, phi, du=v_W)
+    heta_exprnl = fd.NonlinearVariationalSolver(fd.NonlinearVariationalProblem(heta_exprnl2,eta_new,bcs=BC_exclude_beyond_surface))  # ,solver_parameters=param_h) ONNO 19-12: does not work
     # 
 # end if
 #  
@@ -269,8 +299,18 @@ output_data.counter = -1  # -1 to exclude counting print of initial state
 t = 0.0
 i = 0.0
 
-EKin = fd.assemble( 0.5*fd.inner(fd.grad(phi),fd.grad(phi))*fd.dx )
-EPot = fd.assemble( 0.5*gg*fd.inner(eta,eta)*fd.ds(top_id) )
+if nvpcase == 0:
+    EKin = fd.assemble( 0.5*fd.inner(fd.grad(phi),fd.grad(phi))*fd.dx )
+    EPot = fd.assemble( 0.5*gg*fd.inner(eta,eta)*fd.ds(top_id) )
+elif nvpcase == 1:
+    EKin = fd.assemble( 0.5*fd.inner(fd.grad(phi),fd.grad(phi))*fd.dx )
+    EPot = fd.assemble( 0.5*gg*fd.inner(eta,eta)*fd.ds(top_id) )
+elif nvpcase == 2:    
+    EKin = fd.assemble( 1/2 * ( (Lw**2/Ww) * (H0+fac*eta) * (phi.dx(0)-(z/(H0+fac*eta))*fac*eta.dx(0)*phi.dx(1))**2 + Ww * (H0**2/(H0+fac*eta)) * (phi.dx(1))**2) * fd.dx )
+    EPot = fd.assemble( gg*Ww*H0*( 0.5*fd.inner(H0+eta, H0+eta)-fd.inner(H0+eta,H0)+0.5*H0**2) * fd.ds(top_id) )
+    EKin = EKin/(Lw*H0)
+    EPot = EPot/(Lw*H0)
+    
 E = EKin+EPot
 E0 = E
 plt.figure(2)
@@ -282,13 +322,17 @@ plt.xlabel(f'$t$')
 plt.ylabel(f'$E(t)$')
 if nvpcase == 0:
     plt.title(r'Weak form used:',fontsize=tsize)
+    phi_expr.solve() 
 elif nvpcase == 1:
     plt.title(r'Functional derivative VP used:',fontsize=tsize)
+    phi_expr.solve() 
+elif nvpcase == 2:
+    plt.title(r'VP nonlinear used:',fontsize=tsize)
+    
 # end if
 print('E0=',E,EKin,EPot)
 
 
-phi_expr.solve() 
 
 print('Plotting starts, initial data:')
 plt.figure(1)
@@ -299,8 +343,8 @@ ax1.plot(xvals, eta1vals, ':k', label = f' $\eta_n: t = {t:.3f}$',linewidth=2)
 ax2.plot(xvals, phi1vals, ':k', label = f' $\phi_n: t = {t:.3f}$', linewidth=2)
 
 # ONNO 07-12: something is off since t~0 inthe time loop is not close to the initial condition. phi-solve seems wrong; dotted black lines are IC.
-        # ONNO 07-12: something is off since t~0 in the time loop is not close to the initial condition. phi-solve seems wrong; dotted black lines are IC.
-# KOKI 16-12: t += dt happens before plotting, so solution at t = 0 is not plotted in the time loop. Is that why? ONNO 16-12: Saw it already. Fixed
+# ONNO 07-12: something is off since t~0 in the time loop is not close to the initial condition. phi-solve seems wrong; dotted black lines are IC.
+# KOKI 16-12: t += dt happens before plotting, so solution at t = 0 is not plotted in the time loop. Is that why? ONNO 16-12: Saw it already. Fixed but there is still a shift between numercial and exact solution.
 
 output_data()
 print('Time Loop starts')
@@ -327,6 +371,10 @@ while t <= t_end + dt:
         #  mixed variables needs to be assignd ONNO to do?
         #  ONNO is this right:
         # ONNO 06-12 commented out eta_new, phi_new = result_mixed.split() # ONNO: issue is that now only nterior has beemn assigned; how is phi_new at top assigned? Automatically or not?
+    elif nvpcase == 2:
+        phif_exprnl.solve() # solves phi^(n+1) at top free surface same as above
+        phi_exprnl.solve() # solves phi^(n+1) in interior and eta^(n+1) at top surface simulataneously
+        heta_exprnl.solve() 
     # end if
 
     if nvpcase == 0:
@@ -338,10 +386,23 @@ while t <= t_end + dt:
         phi_f.assign(phi)
         phi.assign(phi)
         eta.assign(eta_new)
+    elif nvpcase == 2: # ONNO 19-12
+        phi_f.assign(phi)
+        phi.assign(phi)
+        eta.assign(eta_new)
     # end if
     # Energy monitoring:
-    EKin = fd.assemble( 0.5*fd.inner(fd.grad(phi),fd.grad(phi))*fd.dx )
-    EPot = fd.assemble( 0.5*gg*fd.inner(eta,eta)*fd.ds(top_id) )
+    if nvpcase == 0:
+        EKin = fd.assemble( 0.5*fd.inner(fd.grad(phi),fd.grad(phi))*fd.dx )
+        EPot = fd.assemble( 0.5*gg*fd.inner(eta,eta)*fd.ds(top_id) )
+    elif nvpcase == 1:
+        EKin = fd.assemble( 0.5*fd.inner(fd.grad(phi),fd.grad(phi))*fd.dx )
+        EPot = fd.assemble( 0.5*gg*fd.inner(eta,eta)*fd.ds(top_id) )
+    elif nvpcase == 2:
+        EKin = fd.assemble( 1/2 * ( (Lw**2/Ww) * (H0+fac*eta) * (phi.dx(0)-(z/(H0+fac*eta))*fac*eta.dx(0)*phi.dx(1))**2 + Ww * (H0**2/(H0+fac*eta)) * (phi.dx(1))**2) * fd.dx )
+        EPot = fd.assemble( gg*Ww*H0*(0.5*fd.inner(H0+eta, H0+eta)-fd.inner(H0+eta,H0)+0.5*H0**2) * fd.ds(top_id) )
+        EKin = EKin/(Lw*H0)
+        EPot = EPot/(Lw*H0)
     E = EKin+EPot
     plt.figure(2)
     plt.plot(t,E,'.k')
@@ -371,8 +432,8 @@ while t <= t_end + dt:
         eta_exact_exprv = A * np.cos(kx * xvals) * np.cos(omega * t)
 
         # KOKI: maybe use different markers to distinguish solutions at different times?
-        ax1.plot(xvals, eta_exact_exprv, '-c', linewidth=1)  # ONNO 18-12 still does not look to converge with nvpcase == 0; wrong exact solution or at wrong time?
-        ax2.plot(xvals, phi_exact_exprv, '-c', linewidth=1) # ONNO 18-12 still does not look to converge with nvpcase == 0; wrong exact solution or at wrong time?
+        ax1.plot(xvals, eta_exact_exprv, '-c', linewidth=1) # ONNO 18-12 still does not look to converge with nvpcase == 0,1; wrong exact solution or at wrong time?
+        ax2.plot(xvals, phi_exact_exprv, '-c', linewidth=1) # ONNO 18-12 still does not look to converge with nvpcase == 0,1; wrong exact solution or at wrong time?
         
         ax1.legend(loc=4)
         ax2.legend(loc=4)
